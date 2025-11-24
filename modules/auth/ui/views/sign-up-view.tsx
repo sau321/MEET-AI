db93e96cb9556826982292c3d9447096e5211364
Saurabh Kumar Singh <154623232+sau321@users.noreply.github.com>
2025-11-24 23:25:52 +0530

    Add files via upload

---
+++ modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,238 @@
+"use client";
+import { z } from "zod";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { OctagonAlertIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import Image from "next/image";
+
+const formSchema = z.object({
+    name: z.string().min(1, { message: "Name is required" }),
+    email: z.string().email(),
+    password: z.string().min(1, { message: "Password is required" }),
+    confirmPassword: z.string().min(1, { message: "Confirm Password is required" }),
+})
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords must match",
+        path: ["confirmPassword"],
+    });
+
+export const SignUpView = () => {
+    const router = useRouter();
+
+    const [error, setError] = useState<string | null>(null);
+    const [pending, setPending] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+        },
+    });
+
+    const onSubmit = (data: z.infer<typeof formSchema>) => {
+        setError(null);
+        setPending(true);
+
+        authClient.signUp.email(
+            {
+                name: data.name,
+                email: data.email,
+                password: data.password,
+                callbackURL: "/",
+            },
+            {
+                onSuccess: () => {
+                    setPending(false);
+                    router.push("/");
+                },
+                onError: ({ error }) => {
+                    setError(error.message);
+                }
+            });
+    };
+
+    const onSocial = (provider: "github" | "google") => {
+        setError(null);
+        setPending(true);
+
+        authClient.signIn.social(
+            {
+                provider: provider,
+                callbackURL: "/",
+            },
+            {
+                onSuccess: () => {
+                    setPending(false);
+                },
+                onError: ({ error }) => {
+                    setPending(false);
+                    setError(error.message);
+                }
+            });
+    };
+
+    return (
+        <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm md:max-w-3xl">
+                <Card className="overflow-hidden p-0">
+                    <CardContent className="grid p-0 md:grid-cols-2">
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
+                                <div className="flex flex-col gap-6">
+                                    <div className="flex flex-col items-center text-center">
+                                        <h1 className="text-2xl font-bold">Let&apos;s get started</h1>
+                                        <p className="text-muted-foreground text-balance">
+                                            Create your account
+                                        </p>
+                                    </div>
+                                    <div className="grid gap-3">
+                                        <FormField
+                                            control={form.control}
+                                            name="name"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel className="font-bold">Name</FormLabel>
+                                                    <FormControl>
+                                                        <Input
+                                                            type="text"
+                                                            placeholder="MEET AI"
+                                                            {...field}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </div>
+                                    <div className="grid gap-3">
+                                        <FormField
+                                            control={form.control}
+                                            name="email"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel className="font-bold">Email</FormLabel>
+                                                    <FormControl>
+                                                        <Input
+                                                            type="email"
+                                                            placeholder="a@example.com"
+                                                            {...field}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </div>
+                                    <div className="grid gap-3">
+                                        <FormField
+                                            control={form.control}
+                                            name="password"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel className="font-bold">Password</FormLabel>
+                                                    <FormControl>
+                                                        <Input
+                                                            type="password"
+                                                            placeholder="*********"
+                                                            {...field}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </div>
+                                    <div className="grid gap-3">
+                                        <FormField
+                                            control={form.control}
+                                            name="confirmPassword"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel className="font-bold">Confirm Password</FormLabel>
+                                                    <FormControl>
+                                                        <Input
+                                                            type="password"
+                                                            placeholder="*********"
+                                                            {...field}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </div>
+                                    {!!error && (
+                                        <Alert className="bg-destructive/10 border-none">
+                                            <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
+                                            <AlertTitle>{error}</AlertTitle>
+                                        </Alert>
+                                    )}
+                                    <Button disabled={pending} type="submit" className="w-full font-bold">
+                                        Sign In
+                                    </Button>
+                                    <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:items-center after:border-t">
+                                        <span className="bg-card text-muted-foreground relative z-10 px-2">
+                                            Or continue with
+                                        </span>
+                                    </div>
+                                    <div className="grid grid-cols-2 gap-4">
+                                        <Button disabled={pending} variant="outline" className="w-full" onClick={() => onSocial("google")}>
+                                            <FaGoogle />
+                                            Google
+                                        </Button>
+                                        <Button disabled={pending} variant="outline" className="w-full" onClick={() => onSocial("github")}>
+                                            <FaGithub />
+                                            Github
+                                        </Button>
+                                    </div>
+                                    <div className="text-center text-sm">
+                                        Already have an account?{" "}
+                                        <Link href="/sign-in" className="underline underline-offset-4 font-bold">
+                                            Sign In
+                                        </Link>
+                                    </div>
+                                </div>
+                            </form>
+                        </Form>
+
+                        <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                            <Image
+                                src="/logo.svg"
+                                alt="Logo"
+                                width={92}
+                                height={92}
+                                className="h-[92px] w-[92px]"
+                            />
+                            <p className="text-2xl font-semibold text-white">Meet.AI</p>
+                        </div>
+                    </CardContent>
+                </Card>
+                <br />
+                <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+                    By clicking continue, you agree to our <a href="#" className="font-bold">Terms of Services</a> and <a href="#" className="font-bold">Privacy Policy</a>
+                </div>
+            </div>
+        </div>
+    );
+};
